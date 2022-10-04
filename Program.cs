cdfa51bf79bd7f56cb766cb81be296ce56245ba5
hlengiwe p maseko <hlengiwe p maseko@LAPTOP-ORHN6231>
2022-10-04 19:25:39 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreetTheUser
+{
+    internal class Program
+    {
+        static void SayHi()
+        {
+            Console.Write("What is your name: ");
+            string sName = Console.ReadLine();
+
+            Console.WriteLine("Hello " + sName);
+        }
+        static void Main(string[] args)
+        {
+            //ConsoleColor
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            //Call the SayHi method
+            //Greet the User
+            SayHi();
+
+            //Press any key to exit the program
+            Console.WriteLine("\nPress Any key yo exit the progam...");
+            Console.ReadKey();
+        }
+    }
+}
